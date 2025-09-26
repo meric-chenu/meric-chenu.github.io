9a843687c926a9d2c1da245489facb5c7a12fd15
meric-chenu <meric.chenu@efrei.net>
2025-09-26 16:14:19 +0200

    Add first page

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1758895699',resources: [
+    cacheName: 'chirpy-1758896046',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
